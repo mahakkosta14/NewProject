947506ff31b98194d9fb2f7419aec6fef47e6df2
Mahak kosta <mahakkosta014@gmail.com>
2025-03-08 14:55:18 +0530

    added new project

---
+++ script.js
@@ -0,0 +1,23 @@
+ let body = document.querySelector("body");
+
+function changeColor(color) {
+    body.className = color;
+    console.log(color);
+}
+
+let White = document.querySelector(".white");
+White.addEventListener("click", () => {
+    changeColor("white");
+});
+let black = document.querySelector(".black");
+black.addEventListener("click", () => {
+    changeColor("black");
+});
+let red = document.querySelector(".red");
+red.addEventListener("click", () => {
+    changeColor("red");
+});
+let green = document.querySelector(".green");
+green.addEventListener("click", () => {
+    changeColor("green");
+});
